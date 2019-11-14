0ab3631ce7e9586e1cafd5eb11d286f43ceb9711
william <Williamorenodtd@gmail.com>
2019-11-14 15:53:05 -0300

    Teste de Poo2 (WMC)

---
+++ ProjetoEscolat/src/ControleAluno.java
@@ -0,0 +1,14 @@
+
+public class ControleAluno {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno_1 = new Aluno();
+		Responsavel responsavel_1 = new Responsavel();
+		responsavel_1.setNome("William Moreno Cardoso Moreno Cardoso");
+		aluno_1.setNome("William Moreno Cardoso Moreno Cardoso");
+		aluno_1.setNota1(12.3);
+		aluno_1.setNota2(45.9);
+	}
+
+}
